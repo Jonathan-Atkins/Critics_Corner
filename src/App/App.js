988162e00307d2f639b2bd1121ae580312f0b390
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-02-12 19:43:09 -0800

    feat: down_vote incomplete

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,22 @@
 import './App.css';
-import searchIcon from '../icons/search.png';
+// import searchIcon from '../icons/search.png';
 import Movies from '../MoviesContainer/MoviesContainer';
 import MoviesContainer from '../MoviesContainer/MoviesContainer';
 import moviePosters from '../data/movie_posters';
+import { useState } from 'react';
+
 
 // Example imports (for later):
-// import { useState, useEffect } from 'react';
+
 
 // import movieDetails from '../data/movie_details';
 
 
 function App() {
+
+  // const [movies, setMovies] = useState(movies)
+
+
   return (
     <main className='App'>
       <header>
--- src/MoviePoster/MoviePoster.js
+++ src/MoviePoster/MoviePoster.js
@@ -1,10 +1,19 @@
 import './MoviePoster.css';
+import { useState } from 'react';
 
-function MoviePoster({poster, votes}) {
+function MoviePoster({poster, id, votes}) {
+
+  const [scores, setScores] = useState(votes)
+
+  function downVote(scores){
+    return scores -1
+  };
+  
   return (
     <section className='MoviePosters'>
       <img className="Poster" src={poster} alt="Poster Goes Here"></img>
       <p>{votes}</p>
+      <button onClick = {event => downVote(event)}>👎</button>
     </section>
   );
 }
