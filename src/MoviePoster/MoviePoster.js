0dae67056754429c6c83ba225974905be94f46d7
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-02-13 09:50:02 -0800

    feat:alignment of voteButton

--- src/MoviePoster/MoviePoster.js
+++ src/MoviePoster/MoviePoster.js
@@ -6,8 +6,10 @@ function MoviePoster({poster, id, votes, downVote, upVote}) {
     <section className='MoviePosters'>
       <img className="Poster" src={poster} alt="Poster Goes Here"></img>
       <p>{votes}</p>
+      <section className='voteButtons'> 
       <button onClick={() => downVote(id)}>👎</button>
       <button onClick={() => upVote(id)}>👍</button>
+      </section>
     </section>
   );
 }
