4f9e400ee1051a1db466ad18f7d595721d33bbcd
Paul Knapp <ppankluap@gmail.com>
2025-02-18 19:25:23 -0600

    Feat: cypress installed

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,12 +1,23 @@
 // Mock data to use for testing:
-// import posters from '../fixtures/movie_posters.json' (we've added mock data to this file for you!)
+import MoviePoster from '../../src/MoviePoster/MoviePoster'
+import posters from '../fixtures/movie_posters.json'
 // import details from '../fixtures/movie_details.json' (you will need to add your own mock data to this file!)
 
 describe('Main Page', () => {
   it('displays title on page load', () => {
-    // hint: you'll want to add an intercept here if you are making a network request on page load!
+    cy.intercept('GET', 'https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com/api/v1/movies', {
+      fixture: 'movie_posters.json'
+    })
+
     cy.visit('http://localhost:3000/')
     .get('h1')
-    .contains('rancid tomatillos')
+    .contains('Rancid Tomatillos')
   })
+
+  it ('displays movie posters on page load', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com/api/v1/movies', {
+      fixture: 'movie_posters.json'
+    })
+
+    
 })
