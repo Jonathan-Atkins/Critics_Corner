72aa3c3c239d27b1f75dc0641c8766f573f15ea1
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-02-18 16:21:17 -0800

    feat: Updating voting up and down to render new vote

--- src/MoviesContainer/MoviesContainer.js
+++ src/MoviesContainer/MoviesContainer.js
@@ -2,12 +2,14 @@ import MoviePoster from '../MoviePoster/MoviePoster';
 import './MoviesContainer.css';
 
 function Movies({movies, downVote, upVote}) {
-  if (!movies) {
+  if (!movies || movies.length === 0) {
     return <h2>Loading...</h2>
 }
+
   const movieCards = movies.map( movie => {
     return (
       <MoviePoster
+      key={movie.id}
       id={movie.id}
       poster={movie.poster_path}
       title={movie.title}
