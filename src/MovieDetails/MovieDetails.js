02df34416a82b739c2d2f496529d0e240059218b
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-02-19 21:41:12 -0800

    style:provided more style for background images on MovieDetails

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -1,8 +1,7 @@
-import { data, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import './MovieDetails.css';
 import NavBar from '../NavBar/NavBar';
-
 
 function MovieDetails() {
   const { id } = useParams();
@@ -10,25 +9,31 @@ function MovieDetails() {
 
   const API_URL = `https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com/api/v1/movies/${id}`;
 
-  useEffect(()=> {
-    fetch(`${API_URL}`)  
-    .then(response => response.json())
-    .then(data => setDetails(data))
-    .catch(error => console.log('Error fetching movie details:', error))
-  },[]);
-  console.log("MOVIE ID:", id)
+  useEffect(() => {
+    fetch(`${API_URL}`)
+      .then((response) => response.json())
+      .then((data) => setDetails(data))
+      .catch((error) => console.log('Error fetching movie details:', error));
+  }, []);
 
   if (!details) {
     return <h2>Loading...</h2>;
   }
-  
+
+  const backgroundImage = details.poster_path
+    ? `url(${details.poster_path})`
+    : 'url(/default-background.jpg)';
+
   return (
-    <div>
-      <NavBar/>
-      <section className='MovieDetails'>
-        <h2>{details.title}</h2>
-        <img src={details.poster_path} alt="Movie Poster" />
-        <p>{details.overview}</p>
+    <div className="MovieDetailsContainer" style={{ backgroundImage: backgroundImage }}>
+      <NavBar />
+      <section className="MovieDetails">
+        <div className="BackgroundOverlay"></div>
+        <div className="MovieInfo">
+          <h2>{details.title}</h2>
+          <img src={details.poster_path} alt="Movie Poster" />
+          <p>{details.overview}</p>
+        </div>
       </section>
     </div>
   );
